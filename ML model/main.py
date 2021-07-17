f9a2e0f343d9e7e4a5cf1a606c9b136537ace104
NoahSchiro <noahschiro@rocketmail.com>
2021-07-17 15:48:13 -0400

    Added comments

--- ML model/main.py
+++ ML model/main.py
@@ -4,6 +4,7 @@ import numpy as np
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split
 
+#Allows us to easily pass data into the model.predict() function
 from encodings import *
 
 #Grab the data from the CSV file
@@ -16,10 +17,15 @@ independent_var = nba_data.drop(['game_id', 'pts', 'elo_n', 'opp_pts', 'opp_elo_
 #This is simply pts and opp_pts in an array (what we are trying to predict)
 dependent_var = nba_data[['pts', 'opp_pts']]
 
+#Split the data set into a training and testing set for the purpose of scoring the model
+#In the future we will have 100% of the data go towards training the model
 X_train, X_test, y_train, y_test = train_test_split(independent_var, dependent_var, train_size=0.8)
 
+#Creates the model
 model = linear_model.LinearRegression()
 
+#Trains the model
 model.fit(X_train, y_train)
 
+#Shows us the score
 print(model.score(X_test, y_test))
